f8c475d48112b123e43129710d88177ced6869b8
Liudmila Mihaylichenko <lucy.mihaylichenko@gmail.com>
2023-03-26 15:25:57 +0300

    rechange gitignore

--- app_web.py
+++ app_web.py
@@ -4,7 +4,9 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow.keras.applications.efficientnet import (
+   preprocess_input, decode_predictions
+   )
 
 
 @st.cache(allow_output_mutation=True)
